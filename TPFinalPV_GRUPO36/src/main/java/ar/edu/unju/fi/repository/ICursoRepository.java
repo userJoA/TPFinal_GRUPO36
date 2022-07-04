b88cec7425d85b2aabb3fa53c36b58b26b3d4301
jona <jevkjev@gmail.com>
2022-07-04 15:49:02 -0300

    until de cursos

--- TPFinalPV_GRUPO36/src/main/java/ar/edu/unju/fi/repository/ICursoRepository.java
+++ TPFinalPV_GRUPO36/src/main/java/ar/edu/unju/fi/repository/ICursoRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import ar.edu.unju.fi.entity.Curso;
+//import ar.edu.unju.fi.entity.Curso;
 
-@Repository
-public interface ICursoRepository extends JpaRepository<Curso,Long>{
+//@Repository
+//public interface ICursoRepository extends JpaRepository<Curso,Long>{
 	
-	@Query("from Curso c order by c.titulo")
-	public List<Curso> obtenerCursos();
-	public Curso findByCodigo(int codigo);
+	//@Query("from Curso c order by c.titulo")
+	//public List<Curso> obtenerCursos();
+	//public Curso findByCodigo(int codigo);
 
-}
+//}
--- TPFinalPV_GRUPO36/src/main/java/ar/edu/unju/fi/service/ICursoService.java
+++ TPFinalPV_GRUPO36/src/main/java/ar/edu/unju/fi/service/ICursoService.java
@@ -1,15 +1,16 @@
 package ar.edu.unju.fi.service;
 
-import java.util.List;
 
 import ar.edu.unju.fi.entity.Curso;
+import ar.edu.unju.fi.util.ListaCurso;
+
 
 
 public interface ICursoService {
 	//public void guardarCurso(Curso curso);
 	//public Curso obtenerCurso();
-	public List<Curso> obtenerCursos();
-
+	//public List<Curso> obtenerCursos();
+	public ListaCurso listaCursos();
 	public Curso getCurso();
 	
 	
